c48209ec597a72153e0e4c2270547c9fc09553c4
Caolan Smith <csmith060598@gmail.com>
2025-06-30 10:48:09 +0100

    Updating WIP-Caolan to current working code

--- Front_End/MainMenuForm/Inventory_Database_FrontEnd/obj/Debug/net8.0-windows/Inventory_Database_FrontEnd.AssemblyInfo.cs
+++ Front_End/MainMenuForm/Inventory_Database_FrontEnd/obj/Debug/net8.0-windows/Inventory_Database_FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_Database_FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da616d0f8519d13e7365864dbc0517ce7677ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_Database_FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_Database_FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
